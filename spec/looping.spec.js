b862e8fe096e00270fa191817f5e60e65ffb1e15
popcorn <blah@blah.org>
2013-03-19 16:05:18 +0000

    @aiten @doodlemoonch Finalised looping

--- spec/looping.spec.js
+++ spec/looping.spec.js
@@ -4,52 +4,37 @@ exports.where = function(arguments) {
   clauses.push(arguments);
 };
 
-var executor = function(testMethod, arguments) {
-  testMethod.apply(this, arguments);  
-}
-
-var createDescription = function(description) {
+var createDescription = function(description, items) {
 
   var newDescription = description;
-    for (d=0; d<clauses[i].length; d++) {
-      newDescription = newDescription.replace('#'+d, clauses[i][d]);
-  }
 
+  items.forEach(function(element, index) {
+    newDescription = newDescription.replace('#'+index, element);  
+  });
+
+  return newDescription;
 };
 
 exports.loop = function(description, functionUnderTest) {
 
-  for(i=0; i<clauses.length; i++) {
-    var arguments = [this, functionUnderTest];
-    arguments.push(clauses[i]);
-    it(createDescription(description), executor.apply(this, arguments) );
-  };
+  clauses.forEach(function(element, index) {
+    
+    return it(createDescription(description, element), function() {
+      functionUnderTest.apply(this, element);
+    });
+  });
 
 };
-
-// jasmine.Spec.prototype.chewit = jasmine.Spec.prototype.it;
-// jasmine.Spec.prototype.it = function(description, underlyingFunc) {
-
-//     for(i=0; i<clauses.length; i++) {
-//       var arguments = [functionUnderTest];
-//       arguments.push(clauses[i]);
-//       chewit(createDescription(description), executor.apply(this, arguments));
-//     };  
-
-// }
 
 describe('spec which loops', function() {
 
   describe('Here is some functionality', function() {
 
-    // beforeEach(function() {
-    //   this.expect = jasmine.Spec.expect;
-    // });
-
     exports.where([1, 1]);
     exports.where([2, 2]);
+    exports.where([3, 3]);
 
-    exports.loop('Looping upon an iteration where 2=#1 and 2=#2', function(a, b) {
+    exports.loop('Looping upon an iteration where 2=#0 and 2=#1', function(a, b) {
 
       expect(a).toBe(2);
       expect(b).toBe(2);
