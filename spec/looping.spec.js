3a3562fdd1e8f06438a4ece5c8936c3599caf16b
Antony Jones <adjones+bitbucket@gmail.com>
2013-03-21 12:24:15 +0000

    @aiten Changed DSL to be more readable

--- spec/looping.spec.js
+++ spec/looping.spec.js
@@ -4,11 +4,18 @@ describe('spec which loops', function() {
 
   describe('Here is some functionality', function() {
 
-    neckbeard.where(
-      ['firstNumber', 'secondNumber', 'sum', 'difference'],
-      [[3,            3,              6,              0],
-       [10,           4,              14,             6],
-       [7,            1,              8,              6]]
+    // neckbeard.where(
+    //   ['firstNumber', 'secondNumber', 'sum', 'difference'],
+    //   [[3,            3,              6,              0],
+    //    [10,           4,              14,             6],
+    //    [7,            1,              8,              6]]
+    // );
+
+    neckbeard
+    .iterate('firstNumber', 'secondNumber', 'sum', 'difference')
+    .where(   3,             3,              6,     0,
+              10,            4,              14,    6,
+              7,             1,              8,     6
     );
 
     neckbeard.loop('Hoping that #firstNumber plus #secondNumber is #sum, not #difference', function(firstNumber, secondNumber, sum, difference) {
@@ -20,17 +27,17 @@ describe('spec which loops', function() {
 
   });
 
-  describe('Another quick functionality', function() {
+  // describe('Another quick functionality', function() {
 
-    neckbeard.where(['myVariable'], [[1], [2], [3], [4], [5]])
+  //   neckbeard.where(['myVariable'], [[1], [2], [3], [4], [5]])
 
-    neckbeard.loop('This time, what should happen is 4=#myVariable and #pass', function(theNumber) {
+  //   neckbeard.loop('This time, what should happen is 4=#myVariable and #pass', function(theNumber) {
 
-      expect(theNumber).toBeLessThan(6);
-      expect(theNumber).toBeGreaterThan(0);
+  //     expect(theNumber).toBeLessThan(6);
+  //     expect(theNumber).toBeGreaterThan(0);
 
-    });
+  //   });
 
-  });
+  // });
 
 });
--- spec/neckbeard.js
+++ spec/neckbeard.js
@@ -1,9 +1,35 @@
-var definitions = [];
+var definitions = 0;
 var clauses = [];
 
-exports.where = function(tokens, arguments) {
-  definitions = tokens;
-  clauses = arguments;
+exports.iterate = function() {
+
+      definitions = arguments.length;
+
+      return {
+
+        where: function() {
+
+          valuesOf(arguments).forEach(function(element, index) {
+            var iteration = Math.floor(index/definitions);
+            if (clauses[iteration] === undefined) {
+              clauses[iteration] = [];
+            };
+            clauses[iteration].push(element);
+          });
+
+        }
+      };
+};
+
+var valuesOf = function(object) {
+
+  var values = [];
+
+  for (var key in object) {
+    values.push(object[key]);
+  };
+
+  return values;
 };
 
 var createDescription = function(description, items) {
